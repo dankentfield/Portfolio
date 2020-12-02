3853dc48ffeb27f8c9a78423d5a8f4b9b29b5208
Daniel Kentfield <dan@mymac.com>
2020-12-02 11:25:41 +0300

    Correct styling for button containers and add link

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./index.css";
 import NavBar from "../Navbar";
+import CV from "../../assets/DanielKentfieldCV.pdf";
+
 
 const Header = `final public class Daniel extends FullStackProgrammer`;
 const VIEW_MY_CV = 'VIEW MY CV';
@@ -15,18 +17,21 @@ const Hero = () => {
         <h1 className="header-text">{Header}</h1>
       </div>
 
-      <div className="download-cv-badge-container">
-        <div className="download-cv-badge-container-background">
-          <h3 className="download-cv-badge-text ">{VIEW_MY_CV}</h3>
+      <div className="button-container">
+        <div className="download-cv-badge-container">
+          <a href={CV}>
+            <div className="download-cv-badge-container-background">
+              <h3 className="download-cv-badge-text ">{VIEW_MY_CV}</h3>
+            </div>
+          </a>
+        </div>
+
+        <div className="download-cv-badge-container">
+          <div className="download-cv-badge-container-background">
+            <h3 className="download-cv-badge-text ">{PROJECTS}</h3>
+          </div>
         </div>
       </div>
-
-      <div className="download-cv-badge-container">
-        <div className="download-cv-badge-container-background">
-          <h3 className="download-cv-badge-text ">{PROJECTS}</h3>
-        </div>
-      </div>
-
     </div>
   );
 };
