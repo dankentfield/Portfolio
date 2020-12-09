dbb10ca07935cdfee3f61669e098f60da8be5e50
Daniel Kentfield <dan@mymac.com>
2020-12-09 18:38:57 +0300

    Improve styling and add content to projects.

--- src/Components/Projects/foreign_ears_project.js
+++ src/Components/Projects/foreign_ears_project.js
@@ -26,9 +26,11 @@ const ForeignEarsProject =
       <Header />
       <div className="project-item">
         <div className="project-image-container">
-          <img className="project-screenshot" src={projOne} alt="Screenshot of the gym tracker react native app." />
+          <a href='https://play.google.com/store/apps/details?id=com.foreignears&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' target="_blank">
+            <img className="project-screenshot" src={projOne} alt="Screenshot of the gym tracker react native app." />
+          </a>
           <div className="google-play-badge-container">
-            <a href='https://play.google.com/store/apps/details?id=com.foreignears&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+            <a href='https://play.google.com/store/apps/details?id=com.foreignears&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' target="_blank">
               <img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' className="badge google-play-badge-container" />
             </a>
           </div>
@@ -37,13 +39,13 @@ const ForeignEarsProject =
             <p className="github-text">Code available on request</p>
           </div>
         </div>
-        <div className="project-description-container" >
+        <div className="project-description-container project-one-description-container" >
           <div className='project-one-description-container'>
             <p>Foreign ears helps you to learn a language by helping you understand podcasts in your target language.
               <br/>We do this by generating transcripts for each podcast so you can read and listen at the same time,
               if you find a word you don't understand - tap it to translate it.</p>
             <p>This app was written in React Native and is supported with a complete serverless backend hosted on AWS.
-              <br/>Data is managed using Redux, Redux Saga and Redux persist which is then persisted in the backend.</p>
+              <br/>Data is managed using Redux, Redux Saga and Redux persist which is then persisted in the backend to dynamoDB.</p>
             <div className="tech-stack-container">
               <div className="tech-stack-item">
                 <h2>Frontend: </h2>
--- src/Components/Projects/gym_tracker_project.js
+++ src/Components/Projects/gym_tracker_project.js
@@ -7,7 +7,8 @@ import mongoLogo from "../../assets/images/mongo-logo-white.svg";
 import githubLogo from "../../assets/images/github-logo-dark.png";
 import React from "react";
 
-const githubLinkGymTracker = "https://github.com/dk03/gym-tracker-react-native";
+const githubLinkGymTrackerApp = "https://github.com/dk03/gym-tracker-react-native";
+const githubLinkGymTrackerServer = "https://github.com/dk03/gym-tracker-react-native";
 
 const Header =
   () =>
@@ -30,23 +31,30 @@ const GymTrackerProject =
     <div className="project-item-container white-background">
       <Header />
       <div className="project-item project-reverse">
-        <div className="project-description-container" >
+        <div className="project-description-container project-two-description-container" >
           <div className='project-one-description-container'>
-            <p>MVP Android app written in React Native - for personal use to record workouts in the gym.
-              Hooked into a Node-Express server hosted on AWS EC2 running NGINX.</p>
-            <p>The app persists its data in Mongo Atlas.</p>
+            <p>MVP React Native app.</p>
+            <p>Hooked into a Node-Express server hosted on AWS EC2 running NGINX.</p>
+
+            <p>This is part of a MERN app hosted on AWS.
+              The backend is a MongoDB database hosted on Mongo Atlas, accessed through a Node-Express server (running on an AWS EC2 instance).
+              The initial inspiration for this project was to open up my gym data so I can monitor my fitness and gym performance more closely, importing it into a database for trend analysis (which will form another project).
+            </p>
             <div className="tech-stack-container">
               <div className="tech-stack-item">
                 <h2>Frontend: </h2>
                 <li>React Native</li>
                 <li>Redux</li>
+                <li>React Navigation</li>
+                <li>React native paper (component library)</li>
+                <li>ES6</li>
               </div>
               <div className="tech-stack-item">
-                <h2>Backend: </h2><p></p>
+                <h2>Backend: </h2>
                 <li>AWS EC2</li>
                 <li>NGINX</li>
-                <li>DynamoDB</li>
-                <li>API Gateway</li>
+                <li>NodeJs</li>
+                <li>Express Server</li>
                 <li>MongoDB</li>
               </div>
             </div>
@@ -54,12 +62,21 @@ const GymTrackerProject =
         </div>
         <div className="project-image-container">
            <img className="project-screenshot" src={gymTrackerAndroid} alt="Screenshot of the gym tracker react native app." />
-          <div className="github-container">
-            <a href={githubLinkGymTracker} target="_blank" className="github-container no-style">
-              <img src={githubLogo}/>
-              <p className="github-text">View code here</p>
-            </a>
-          </div>
+
+              <div className="github-group-container">
+                <div className="github-container">
+                    <a href={githubLinkGymTrackerApp} target="_blank" className="github-container no-style">
+                      <img src={githubLogo}/>
+                      <p className="github-text">App code here</p>
+                    </a>
+                 </div>
+                <div className="github-container">
+                    <a href={githubLinkGymTrackerServer} target="_blank" className="github-container no-style">
+                      <img src={githubLogo}/>
+                      <p className="github-text">Server code here</p>
+                    </a>
+                  </div>
+              </div>
         </div>
       </div>
     </div>);
